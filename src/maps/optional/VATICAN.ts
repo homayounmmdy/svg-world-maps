1155340b54321276b1afb7fbf55873b04e688ebf
Homayoun <homayoun763@gmail.com>
2026-07-22 19:59:03 +0330

    add : Vatican svg map

---
+++ src/maps/optional/VATICAN.ts
@@ -0,0 +1,24 @@
+const VATICAN = {
+    name: "Vatican",
+    code: "VA",
+    viewBox: "0 0 1000 1000",
+    states: [
+        {
+            name: "Vatican",
+            code: "VAVA",
+            path:                 d="M361.6 954.5l-246.1-206.6 30.8-330.6 153.8-371.8 553.6 41.2 30.8 867.8-522.9 0z"
+
+        },
+
+    ],
+    labels: [
+        {
+            code: "VAVA",
+            x: "525",
+            y: "508.4",
+            name: "Vatican"
+        },
+    ]
+}
+
+export default VATICAN
