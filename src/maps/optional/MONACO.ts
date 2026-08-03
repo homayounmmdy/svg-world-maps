9f6007b2c229532cc855e6ac62903c65bbcfa151
Homayoun <homayoun763@gmail.com>
2026-08-04 00:40:42 +0330

    add : monaco svg map

---
+++ src/maps/optional/MONACO.ts
@@ -0,0 +1,22 @@
+const MONACO = {
+    name: "Monaco",
+    code: "MC",
+    viewBox: "0 0 1000 879",
+    states: [
+        {
+            name: "Monaco",
+            code: "MC",
+            path: "M954.5 393.5l-58.4 61.6-188.7 157-173 226.9-299.1-22.8-189.8-60.7 19.1-199.9 68.4-205.4 188.7-211.8 246.3-98.4 244.4 141.1 142.1 212.4z"
+        },
+    ],
+    labels: [
+        {
+            code: "MC-MC",
+            x: "458",
+            y: "458.6",
+            name: "temp"
+        },
+    ]
+}
+
+export default MONACO
