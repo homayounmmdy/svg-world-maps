6c3bad8f291d51df4bac2f2eb7dc14420c6ee8bb
Homayoun <homayoun763@gmail.com>
2026-08-15 21:03:27 +0330

    add : 19 new countries into list of maps

---
+++ src/maps/optional/SAINT_MARTIN_FRENCH.ts
@@ -0,0 +1,22 @@
+const SAINT_MARTIN_FRENCH = {
+    name: "Saint_martin_french",
+    code: "MF",
+    viewBox: "0 0 1000 686",
+    states: [
+        {
+            name: "St-Martin",
+            code: "MF",
+            path: "M908.9 654.7l-456.3-176.5-141.1-25.3 0-34-55.4 11.1-101.7 36-25.5 5.5-63.6-36.6-19.8-39.5 21.4-27.1 199.5-3.5 118-29.1 82-64.3 31.6-114.1 54.9-40.9 125.2-48 138.4-35.5 91.8-1.7-1.9 151.8 45.7 209.3 2.4 210.1-45.6 52.3z"
+        }
+    ],
+    labels: [
+        {
+            code: "MF",
+            x: "696.3",
+            y: "318.1",
+            name: "St-Martin"
+        }
+    ]
+}
+
+export default SAINT_MARTIN_FRENCH
