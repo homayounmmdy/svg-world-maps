ded9359c82567ec6063e076b7e7166a43ace94ae
Homayoun <homayoun763@gmail.com>
2026-08-15 21:56:05 +0330

    add : 13 new countries into list of maps

---
+++ src/maps/optional/SAINT_KITTS_AND_NEVIS.ts
@@ -0,0 +1,165 @@
+const SAINT_KITTS_AND_NEVIS = {
+    name: "Saint_kitts_and_nevis",
+    code: "KN",
+    viewBox: "0 0 1000 1000",
+    states: [
+        {
+            name: "Saint Paul Charlestown",
+            code: "KN10",
+            path: "M713 874.5l-13.2-35.7 81.4 6.8 50-20.6-37.5 37.4-44.6 9.3-36.1 2.8z"
+        },
+        {
+            name: "Saint John Figtree",
+            code: "KN07",
+            path: "M713 874.5l36.1-2.8 44.6-9.3 37.5-37.4 35.5 129.5-127.8-10.4-25.9-69.6z"
+        },
+        {
+            name: "Saint George Gingerland",
+            code: "KN04",
+            path: "M866.7 954.5l-35.5-129.5 44.7-13.1 37.5 13.1 32.6 20.5 0.5 25-4.6 25.1-14.1 34.9-11.1 12.2-50 11.8z"
+        },
+        {
+            name: "Saint James Windward",
+            code: "KN05",
+            path: "M946 845.5l-32.6-20.5-37.5-13.1-44.7 13.1-46.4-91.6-47-34.1 36.7-39.9 57.2 1.2 49.8 16.8 38.2 37.3 22.2 62.9 3.2 26.1 0.9 41.8z"
+        },
+        {
+            name: "Saint Thomas Lowland",
+            code: "KN12",
+            path: "M831.2 825l-50 20.6-81.4-6.8 21.2-121.3 16.8-18.2 47 34.1 46.4 91.6z"
+        },
+        {
+            name: "Saint Paul Capesterre",
+            code: "KN09",
+            path: "M65.8 147.6l9.6-41.2 10.7-9.9 7.4-5.3 8.2-3.1 13-3.2 33.6-14.4 11.9-8.5 10.2 24.1 0 28.1 1.7 33.7 9 16.8 17.8 5.7 1.8 16.8-78.6-16.8 3.6-20.6-16.1-7.5-10.7 15-33.1-9.7z"
+        },
+        {
+            name: "Saint Anne Sandy Point",
+            code: "KN02",
+            path: "M65.8 147.6l33.1 9.7 10.7-15 16.1 7.5-3.6 20.6 78.6 16.8-19.6 39.3-12.5 0-14.3 33.7-19.6 32.5-81.2-92.7 12.3-52.4z"
+        },
+        {
+            name: "Saint Thomas Middle Island",
+            code: "KN13",
+            path: "M134.7 292.7l19.6-32.5 14.3-33.7 12.5 0 19.6-39.3 55.4 48.7 19.6 22.4 3.6 18.7 67.9 13.1-10.7 39.3-21.5 11.2-8.9 24.4-6.3 20.3-47.1-19.2-118-73.4z"
+        },
+        {
+            name: "Trinity Palmetto Point",
+            code: "KN14",
+            path: "M299.8 385.3l6.3-20.3 8.9-24.4 21.5-11.2 10.7-39.3 25 0 19.6 1.9 10.8 33.7 14.2 28-7.1 9.4 21.8 40.5-55.7 3.1-35.6-4.9-40.4-16.5z"
+        },
+        {
+            name: "Saint George Basseterre",
+            code: "KN03",
+            path: "M431.5 403.6l-21.8-40.5 7.1-9.4-14.2-28 17.8-5.6 12.5 20.5 37.5 7.5 28.6 15 25 20.6 24.9-2.3 8.3 20.6 31.1 54 45.1 22.8 37.6 21.3 33 47.2 0.2 47.7-61.4 23.4-34.9-24-29.6-108.6-54.5-34-36-40.8-25.3-9.2-31 1.8z"
+        },
+        {
+            name: "Saint Peter Basseterre",
+            code: "KN11",
+            path: "M402.6 325.7l-10.8-33.7 17.9-24.3 26.8 3.7 28.6 0 22.1-11.8 22.3 24.2 39.4 97.6-24.9 2.3-25-20.6-28.6-15-37.5-7.5-12.5-20.5-17.8 5.6z"
+        },
+        {
+            name: "Saint Mary Cayon",
+            code: "KN08",
+            path: "M391.8 292l-19.6-1.9-1.8-37.4 12.5-29.9 10.7-18.8 19.4-10.7 29.1 17.5 45.1 48.8-22.1 11.8-28.6 0-26.8-3.7-17.9 24.3z"
+        },
+        {
+            name: "Christ Church Nichola Town",
+            code: "KN01",
+            path: "M347.2 290.1l-67.9-13.1-3.6-18.7-19.6-22.4 33.9-50.6-3.5-20.6 19.4-36 107.1 64.6-19.4 10.7-10.7 18.8-12.5 29.9 1.8 37.4-25 0z"
+        },
+        {
+            name: "Saint John Capesterre",
+            code: "KN06",
+            path: "M200.7 187.2l-1.8-16.8-17.8-5.7-9-16.8-1.7-33.7 0-28.1-10.2-24.1 7.6-16.5 138.1 83.2-19.4 36 3.5 20.6-33.9 50.6-55.4-48.7z"
+        }
+    ],
+    labels: [
+        {
+            code: "KN10",
+            x: "747.8",
+            y: "857.3",
+            name: "Saint Paul Charlestown"
+        },
+        {
+            code: "KN07",
+            x: "794.1",
+            y: "905.3",
+            name: "Saint John Figtree"
+        },
+        {
+            code: "KN04",
+            x: "895.7",
+            y: "876.4",
+            name: "Saint George Gingerland"
+        },
+        {
+            code: "KN05",
+            x: "860.9",
+            y: "743.4",
+            name: "Saint James Windward"
+        },
+        {
+            code: "KN12",
+            x: "758.2",
+            y: "786.7",
+            name: "Saint Thomas Lowland"
+        },
+        {
+            code: "KN09",
+            x: "137.1",
+            y: "114.8",
+            name: "Saint Paul Capesterre"
+        },
+        {
+            code: "KN02",
+            x: "122.1",
+            y: "213.2",
+            name: "Saint Anne Sandy Point"
+        },
+        {
+            code: "KN13",
+            x: "228.4",
+            y: "286.3",
+            name: "Saint Thomas Middle Island"
+        },
+        {
+            code: "KN14",
+            x: "367.3",
+            y: "359.7",
+            name: "Trinity Palmetto Point"
+        },
+        {
+            code: "KN03",
+            x: "641.3",
+            y: "543.1",
+            name: "Saint George Basseterre"
+        },
+        {
+            code: "KN11",
+            x: "478.2",
+            y: "308.3",
+            name: "Saint Peter Basseterre"
+        },
+        {
+            code: "KN08",
+            x: "414.8",
+            y: "234.1",
+            name: "Saint Mary Cayon"
+        },
+        {
+            code: "KN01",
+            x: "330.1",
+            y: "218.8",
+            name: "Christ Church Nichola Town"
+        },
+        {
+            code: "KN06",
+            x: "240.6",
+            y: "145.4",
+            name: "Saint John Capesterre"
+        }
+    ]
+}
+
+export default SAINT_KITTS_AND_NEVIS
